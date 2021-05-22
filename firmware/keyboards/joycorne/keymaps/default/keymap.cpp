77de900de59f274428d86658be7280840cfbaec3
Leo <dias.lopes.leo@gmail.com>
2021-05-22 23:07:59 +0100

    :construction_worker: Add thumbstick code to joycorne

--- firmware/keyboards/joycorne/keymaps/default/keymap.cpp
+++ firmware/keyboards/joycorne/keymaps/default/keymap.cpp
@@ -17,31 +17,86 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 */
 #include "keymap.h"
 
-#define THUMBSTICK_X_PIN  29
-#define THUMBSTICK_Y_PIN  2
-
-#if KEYBOARD_SIDE == SINGLE
-std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
-    {KEYMAP(
-            KC_1,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
-            KC_2,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-            KC_3,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-                            KC_3,     KC_4,    KC_5,    KC_6
-    )};
-
-void setupKeymap() {
-
-   // no layers for SINGLE keymap
-   // this is a keymap that's used for testing that each key is responding properly to key presses
-   // flash this keymap to both left and right to test whether each half works properly.
-   // once tested, you can flash the left and right to their respective halves.
-
-}
-#endif
-
-
 #if KEYBOARD_SIDE == LEFT
+#define THUMBSTICK_READ_INTERVAL_MS 50
+#define THUMBSTICK_X_PIN 31
+#define THUMBSTICK_Y_PIN 2
 
+#define THUMBSTICK_X_FLIP true
+#define THUMBSTICK_Y_FLIP false
+
+#define THUMBSTICK_DEAD_ZONE 90 // Values below this are ignored (deadzone)
+#define THUMBSTICK_SPEED 256
+
+// Implicit and derived constants
+#define THUMBSTICK_RANGE_START 0
+#define THUMBSTICK_RANGE_STOP 1023
+#define THUMBSTICK_RANGE_CENTER (THUMBSTICK_RANGE_STOP - THUMBSTICK_RANGE_START + 1) / 2
+#define THUMBSTICK_RANGE_MOVEMENT (THUMBSTICK_RANGE_CENTER - THUMBSTICK_DEAD_ZONE)
+
+
+BLEHidAdafruit blehid;
+
+// Axis-level wrapper to read raw value
+uint16_t thumbstick_get_axis_distance(uint16_t pin)
+{
+    uint16_t analogValue = analogRead(pin);
+    // Compute direction
+    bool directionIsPositive = (analogValue > THUMBSTICK_RANGE_CENTER);
+    // Compute distance from the center
+    uint16_t distance = directionIsPositive ? (analogValue - THUMBSTICK_RANGE_CENTER) : (THUMBSTICK_RANGE_CENTER - analogValue);
+    // Compute component (range of [0 to 1023])
+    return directionIsPositive ? distance : -(uint16_t)distance;
+}
+
+int16_t thumbstick_get_mouse_speed(int16_t distance)
+{
+    int16_t maxSpeed;
+    uint16_t abs_distance = abs(distance);
+    // if (distance > THUMBSTICK_FINE_ZONE) {
+    //     maxSpeed = THUMBSTICK_SPEED;
+    // } else
+    if (abs_distance > THUMBSTICK_DEAD_ZONE)
+    {
+        maxSpeed = THUMBSTICK_SPEED; // can be higher or lower
+    }
+    else
+    {
+        return 0;
+    }
+    return (float)maxSpeed * distance / THUMBSTICK_RANGE_CENTER;
+}
+
+// Axis-level wrapper to get corresponding mouse move
+int16_t thumbstick_get_axis_mouse_move(int16_t pin, bool flip_axis)
+{
+    int16_t axis_distance;
+    if (flip_axis)
+    {
+        axis_distance = -thumbstick_get_axis_distance(pin);
+    }
+    else
+    {
+        axis_distance = thumbstick_get_axis_distance(pin);
+    }
+    return thumbstick_get_mouse_speed(axis_distance);
+}
+
+// Interval between systick event
+extern "C"
+{
+
+    void SysTick_Handler(void)
+    {
+        int16_t x_move;
+        int16_t y_move;
+
+        x_move = thumbstick_get_axis_mouse_move(THUMBSTICK_X_PIN, THUMBSTICK_X_FLIP);
+        y_move = thumbstick_get_axis_mouse_move(THUMBSTICK_Y_PIN, THUMBSTICK_Y_FLIP);
+
+        blehid.mouseMove(x_move, y_move);
+    }
+} // extern C
 /* Qwerty
  * ,-----------------------------------------.
  * | Tab  |   Q  |   W     |   E  |   R  |   T  |
@@ -56,16 +111,15 @@ void setupKeymap() {
 
 std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
     {KEYMAP(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,
-                          KC_LALT, KC_LGUI, LAYER_1,  KC_SPC
-    )};
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,
+        KC_LALT, KC_LGUI, LAYER_1, KC_SPC)};
 
+void setupKeymap()
+{
 
-void setupKeymap() {
-
-/* Layer 1 (Raise)
+    /* Layer 1 (Raise)
  * ,-----------------------------------------.
  * | ESC  |   1  |   2  |   3  |   4  |   5  |
  * |------+------+------+------+------+------|
@@ -77,12 +131,11 @@ void setupKeymap() {
  * `-----------------------------------------'
  */
     uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-              KC_ESC,   PRINT_BATTERY, BLEPROFILE_1,    KC_3,    KC_4,    KC_5, \
-              PRINT_INFO,  OUT_AUTO,     OUT_USB,  OUT_BT,   KC_NO,   KC_NO, \
-              PRINT_BLE,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, \
-                                 KC_LALT, KC_LGUI, LAYER_1, KC_SPC \
-        );
+        KEYMAP(
+            KC_ESC, PRINT_BATTERY, BLEPROFILE_1, KC_3, KC_4, KC_5,
+            PRINT_INFO, OUT_AUTO, OUT_USB, OUT_BT, KC_NO, KC_NO,
+            PRINT_BLE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_LALT, KC_LGUI, LAYER_1, KC_SPC);
 
     /* Layer 2 (lower)
  * ,-----------------------------------------.
@@ -96,12 +149,11 @@ void setupKeymap() {
  * `-----------------------------------------'
  */
     uint32_t layer2[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-              KC_ESC,  KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC, \
-              KC_LCTL, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO, \
-              KC_LSFT, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO, \
-                                KC_LALT,   KC_LGUI, LAYER_1, KC_SPC \
-        );
+        KEYMAP(
+            KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,
+            KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_LALT, KC_LGUI, LAYER_1, KC_SPC);
     /*
      * add the other layers on the regular presses.
      */
@@ -114,23 +166,19 @@ void setupKeymap() {
         }
     }
 
-    // if you want to add Tap/Hold or Tap/Doubletap activations, then you add them below.
-
-    Serial.begin(115200);
     pinMode(THUMBSTICK_X_PIN, INPUT);
     pinMode(THUMBSTICK_Y_PIN, INPUT);
+    /* Input parameter is number of ticks between interrupts handler i.e SysTick_Handler
+   * 1000 ms --> F_CPU            ticks
+   * T    ms --> (F_CPU/1000)*T   ticks
+   *
+   * Note: Since systick is 24-bit timer, the max tick value is 0xFFFFFF, F_CPU = 64 Mhz
+   * --> our Tmax = 0xFFFFFF/64000 ~ 262 ms
+   */
+
+    SysTick_Config( (F_CPU/1000)*THUMBSTICK_READ_INTERVAL_MS);
 }
-
-
-void process_keyboard_function(uint16_t keycode)
-{
-    Serial.println(keycode);
-    addStringToQueue("hello");
-
-}
-#endif  // left
-
-
+#endif // left
 
 #if KEYBOARD_SIDE == RIGHT
 
@@ -148,15 +196,15 @@ void process_keyboard_function(uint16_t keycode)
 
 std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
     {KEYMAP(
-            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_BSPACE,
-            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCOLON, KC_QUOTE,
-            KC_N,    KC_M,    KC_COMMA,KC_DOT,  KC_SLSH,   KC_RSFT,
-            KC_ENT,  LAYER_2, KC_RALT, KC_RGUI
-    )};
+        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPACE,
+        KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,
+        KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_ENT, LAYER_2, KC_RALT, KC_RGUI)};
 
-void setupKeymap() {
+void setupKeymap()
+{
 
-/* Layer 1 (Raise)
+    /* Layer 1 (Raise)
  * ,-----------------------------------------.
  * |   Y  |   U  |   I  |   O  |   P  | Del  |
  * |------+------+------+------+-------------|
@@ -168,12 +216,11 @@ void setupKeymap() {
  * `-----------------------------------------'
  */
     uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-              KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_BSPC, \
-              KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_NO,   KC_NO, \
-              KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO, \
-              KC_SPC,  LAYER_2, KC_RALT, KC_RGUI  \
-        );
+        KEYMAP(
+            KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+            KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_NO, KC_NO,
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+            KC_SPC, LAYER_2, KC_RALT, KC_RGUI);
 
     /* Layer 2 (lower)
  * ,-----------------------------------------.
@@ -187,12 +234,11 @@ void setupKeymap() {
  * `-----------------------------------------'
  */
     uint32_t layer2[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP( \
-                KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-                KC_MINS,  KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV, \
-                KC_UNDS,  KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD, \
-                KC_ENT,   LAYER_2, KC_RALT, KC_RGUI  \
-        );
+        KEYMAP(
+            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+            KC_MINS, KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV,
+            KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
+            KC_ENT, LAYER_2, KC_RALT, KC_RGUI);
     /*
      * add the other layers
      */
@@ -204,8 +250,6 @@ void setupKeymap() {
             matrix[row][col].addActivation(_L2, Method::PRESS, layer2[row][col]);
         }
     }
-
 }
 
 #endif
-
